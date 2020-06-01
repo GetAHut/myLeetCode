bf66475e2e6c294d2d411959464230cf18ab77c3
cherryLoveAbby <184063657@qq.com>
2020-06-01 11:24:12 +0800

    leetcode

---
+++ src/main/java/com/xyt/leecode/LeecodeApplication.java
@@ -0,0 +1,13 @@
+package com.xyt.leecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeecodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyt/leecode/LeecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyt.leecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeecodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
